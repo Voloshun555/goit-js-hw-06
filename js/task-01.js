9ebd0f4c370c0269965273e80c21bb0f58cbff6c
Voloshun555 <voloshunihor@gmail.com>
2023-03-06 21:03:18 +0200

    23

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,29 @@
-const listOfCategories = document.querySelector("#categories");
-const arryListOfCategories = listOfCategories.children;
-console.log('Number of categories: ', arryListOfCategories.length);
+// const listOfCategories = document.querySelector("#categories");
+// const arryListOfCategories = listOfCategories.children;
+// console.log('Number of categories: ', arryListOfCategories.length);
 
-const ListAnimals = arryListOfCategories[0].children;
-console.log('Category: ', ListAnimals[0].textContent);
-console.log('Elements: ', ListAnimals[1].children.length);
+// const ListAnimals = arryListOfCategories[0].children;
+// console.log('Category: ', ListAnimals[0].textContent);
+// console.log('Elements: ', ListAnimals[1].children.length);
 
 
-const ListProduct = arryListOfCategories[1].children;
-console.log('Category: ', ListProduct[0].textContent)
-console.log('Elements: ', ListProduct[1].children.length);
+// const ListProduct = arryListOfCategories[1].children;
+// console.log('Category: ', ListProduct[0].textContent)
+// console.log('Elements: ', ListProduct[1].children.length);
 
-const ListTechnologies = arryListOfCategories[2].children;
-console.log('Category: ', ListTechnologies[0].textContent)
-console.log('Elements: ', ListTechnologies[1].children.length);
+// const ListTechnologies = arryListOfCategories[2].children;
+// console.log('Category: ', ListTechnologies[0].textContent)
+// console.log('Elements: ', ListTechnologies[1].children.length);
 
 // const list = document.querySelectorAll('.item');
 // console.log(list)
+
+
+const listOfCategories = document.querySelectorAll('.item');
+console.log('Number of categories: ', listOfCategories.length);
+
+
+listOfCategories.forEach(element => {
+console.log("Category:", element.firstElementChild.textContent);
+console.log("Elements:", element.lastElementChild.childElementCount)
+})
